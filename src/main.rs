3931da3eecf26a08d1aa351d61b4eb57e063572c
Clément Guiton <clement.guiton.dev@gmail.com>
2024-04-12 12:37:00 +0200

    test read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::time::{Duration, Instant};
+
+mod read_file;
+// Function to read a file
 fn main() {
-    println!("Hello, world!");
+    // We read the file a first time to warm up the cache
+
+    let start = Instant::now();
+    read_file::read_file("logs.txt").expect("Could not read file");
+    let duration = start.elapsed();
+    println!("Time elapsed warmup is: {:?}", duration);
+
+    let nb_iter = 1000;
+    let serial_mean = (0..nb_iter)
+        .map(|_| bench_serial())
+        .fold(Duration::ZERO, |acc, x| acc + x)
+        / nb_iter;
+
+    println!(
+        "Mean time elapsed in read_file_serial() is: {:?}",
+        serial_mean
+    );
+
+    let parallel_mean = (0..nb_iter)
+        .map(|_| bench_parallel())
+        .fold(Duration::ZERO, |acc, x| acc + x)
+        / nb_iter;
+    println!(
+        "Mean time elapsed in read_file_parallel() is: {:?}",
+        parallel_mean
+    );
+}
+
+fn bench_serial() -> std::time::Duration {
+    let start = Instant::now();
+    read_file::read_serial();
+    start.elapsed()
+}
+
+fn bench_parallel() -> std::time::Duration {
+    let start = Instant::now();
+    read_file::read_para();
+    start.elapsed()
 }
---
+++ src/read_file.rs
@@ -0,0 +1,37 @@
+use rayon::prelude::*;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+pub fn read_file(filename: &str) -> io::Result<BufReader<File>> {
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file))
+}
+
+pub fn read_para() {
+    let target_word = "error";
+
+    let reader = read_file("logs.txt").expect("Could not read file");
+
+    let error_count: usize = reader
+        .lines()
+        .filter_map(Result::ok)
+        .par_bridge()
+        .filter(|line| {
+            line.contains(target_word) // This is a stand in  for expensive line processing
+        })
+        .count();
+}
+
+pub fn read_serial() {
+    let target_word = "error";
+
+    let reader = read_file("logs.txt").expect("Could not read file");
+
+    let error_count: usize = reader
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|line| {
+            line.contains(target_word) // This is a stand in  for expensive line processing
+        })
+        .count();
+}
